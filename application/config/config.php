16be6004bb37a0d2c705a80127e3bfdd78a8f8da
Shubhajit Saha <suvozit@live.com>
2015-11-03 16:44:58 +0530

    Configuring CI

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/email';
 
 /*
 |--------------------------------------------------------------------------
@@ -314,7 +314,7 @@ $config['cache_query_string'] = FALSE;
 | http://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'b48a5a7f1e0981ed856aacd9ff594ced';
 
 /*
 |--------------------------------------------------------------------------
