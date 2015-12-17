e5841a637f8907e26fc2c98fbc56665590024c96
Shubhajit Saha <suvozit@live.com>
2015-12-17 06:05:23 +0530

    Update nav.php

--- application/config/nav.php
+++ application/config/nav.php
@@ -11,18 +11,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config = [
   'navbar' => [
-    'left' => [
-      'stats'         => '<span class="glyphicon glyphicon-stats"></span>',
-      'message'       => '<span class="glyphicon glyphicon-envelope"></span>',
-      's3'            => '<span class="glyphicon glyphicon-open-file"></span>',
-      'autoresponder' => 'Autoresponder',
-      'campaign'      => 'Campaign',
-      'transaction'   => 'Transaction',
-      'settings'      => '<span class="glyphicon glyphicon-cog"></span>',
-    ],
-    'right' =>[
-      'auth/sign-out' => 'Sign out',
-    ]
+    'stats'         => '<span class="glyphicon glyphicon-stats"></span>',
+    'message'       => '<span class="glyphicon glyphicon-envelope"></span>',
+    's3'            => '<span class="glyphicon glyphicon-open-file"></span>',
+    'autoresponder' => 'Autoresponder',
+    'campaign'      => 'Campaign',
+    'transaction'   => 'Transaction',
+    'settings'      => '<span class="glyphicon glyphicon-cog"></span>',
   ],
   'nav_tab' => [
     's3' => [
