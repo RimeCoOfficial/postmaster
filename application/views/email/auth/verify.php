020df39e24ab0db50a84adf79d069c46d4e9f8ee
Shubhajit Saha <suvozit@live.com>
2016-01-17 18:48:21 +0530

    Html verify email 😡😡😡

--- application/views/email/auth/verify.php
+++ application/views/email/auth/verify.php
@@ -2,7 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<?php echo anchor('auth/verify/'.$login_email_key, NULL, 'target="_blank"'); ?>
+<p>
+ <?php echo anchor('auth/verify/'.$login_email_key, NULL, 'target="_blank"'); ?>
+</p>
 
-Happy emailing!
-&mdash; The <?php echo app_name(); ?>
+<p>
+  Happy emailing!
+  &mdash; The <?php echo app_name(); ?>
+</p>
