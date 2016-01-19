11a60804f381b423bc946c8f63791c8c6117231f
Shubhajit Saha <suvozit@live.com>
2016-01-19 18:17:39 +0530

    SEO: Meta Description tag #1

--- application/views/base.php
+++ application/views/base.php
@@ -14,7 +14,8 @@ CodeIgniter Version <?php echo CI_VERSION; ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Postmaster</title>
+    <title><?php echo app_name(); ?></title>
+    <meta name="description" content="AWS email server">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"> -->
     <style type="text/css">
