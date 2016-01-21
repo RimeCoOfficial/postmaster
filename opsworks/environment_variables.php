10695f3403f59cf8d761530224174c0315fa2998
Shubhajit Saha <suvozit@live.com>
2016-01-21 20:52:30 +0530

    Default env var

--- opsworks/environment_variables.php
+++ opsworks/environment_variables.php
@@ -13,9 +13,9 @@ putenv('db_password=root');
 
 // Debug keys
 putenv('AWS_PHP_CACHE_DIR=/tmp/aws-cache');
-putenv('aws_account_id=667785696107');
-putenv('aws_access_key=AKIAIASESUE7XE4LZRQA');
-putenv('aws_secret_key=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');
+putenv('aws_account_id=111111111111');
+putenv('aws_access_key=XXXXXXXXXXXXXXXXXXXX');
+putenv('aws_secret_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
 
 // US East (N. Virginia)        us-east-1
 // US West (Oregon)             us-west-2
@@ -26,14 +26,14 @@ putenv('aws_secret_key=djOrYWadwZatSrCz2sSMTACavOUzA2n90YZ91LY5');
 // Asia Pacific (Sydney)        ap-southeast-2
 // Asia Pacific (Tokyo)         ap-northeast-1
 // South America (Sao Paulo)    sa-east-1
-putenv('aws_region=us-east-1');
-putenv('aws_s3_bucket=rime-mail');
+putenv('aws_region=xx-xxxx-1');
+putenv('aws_s3_bucket=bucket');
 
-putenv('ga=UA-XXXXXXXXX-X');
+putenv('ga=UA-XXXXXXXXX-1');
 
 putenv('email_source=postmaster@example.com');
 
-putenv('email_admin=postmaster@example.com');
+putenv('email_admin=admin@example.com');
 putenv('email_debug=debug-postmaster@example.com');
 
-putenv('api_key=e07e342abbbf4215b84ce93abafe2cbd');
+putenv('api_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
