f0a90976fb90812027b3c04cc0644f450e519d4a
Shubhajit Saha <suvozit@live.com>
2016-01-31 02:19:22 +0530

    Scripts crossorigin="anonymous"

--- application/views/base.php
+++ application/views/base.php
@@ -11,12 +11,17 @@ CodeIgniter Version <?php echo CI_VERSION; ?>
  -->
 <html lang="en">
   <head>
+    <title><?php echo app_name(); ?> - AWS email server</title>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo app_name(); ?></title>
-    <meta name="description" content="AWS email server">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <meta name="title" content="<?php echo app_name(); ?> - AWS email server">
+    <meta name="description" content="🍳 Fork: https://github.com/RimeOfficial/postmaster 🍺 Wiki: https://github.com/RimeOfficial/postmaster/wiki">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"> -->
     <style type="text/css">
       body { padding-top: 20px; padding-bottom: 20px; }
@@ -110,6 +115,7 @@ CodeIgniter Version <?php echo CI_VERSION; ?>
       </div>
     </div>
     
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </body>
 </html>
