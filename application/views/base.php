a9a783a4c2b4c4e402f467f5dcae3a8d1a40ad00
Shubhajit Saha <suvozit@live.com>
2016-01-31 02:40:50 +0530

    a little bit of optimisation :P

--- application/views/base.php
+++ application/views/base.php
@@ -11,24 +11,30 @@ CodeIgniter Version <?php echo CI_VERSION; ?>
  -->
 <html lang="en">
   <head>
-    <title><?php echo app_name(); ?> - AWS email server</title>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title><?php echo app_name(); ?> - AWS email server</title>
     <meta name="title" content="<?php echo app_name(); ?> - AWS email server">
     <meta name="description" content="🍳 Fork: https://github.com/RimeOfficial/postmaster 🍺 Wiki: https://github.com/RimeOfficial/postmaster/wiki">
 
+    <!-- Bootstrap -->
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"> -->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
     <style type="text/css">
       body { padding-top: 20px; padding-bottom: 20px; }
       .container { max-width: 680px; }
     </style>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
   </head>
   <body>
    
@@ -115,6 +121,10 @@ CodeIgniter Version <?php echo CI_VERSION; ?>
       </div>
     </div>
     
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </body>
