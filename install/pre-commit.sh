67485c2af1a2e4cbd7178d29891d6565cda55829
Shubhajit Saha <suvozit@live.com>
2016-02-04 13:16:00 +0530

    empty opsworks/*

--- install/pre-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "1. Untrack environment vars"
-
-# cd ~/Sites/postmaster
-git update-index --assume-unchanged opsworks/*
-# git update-index --no-assume-unchanged opsworks/*
-
-exit 0
