7ca62ec39ca2426076e3f93f46edeb10ca9014e0
Shubhajit Saha <suvozit@live.com>
2016-02-19 05:32:57 +0530

    aah not required

--- application/views/sign_in.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-
-<form action="?" method="POST">
-  <div class="g-recaptcha" data-sitekey="your_site_key"></div>
-  <br/>
-  <input type="submit" value="Submit">
-</form>
