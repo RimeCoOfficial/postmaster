0f65a41568811225f804b9570d53d474346b8f2d
Shubhajit Saha <suvozit@icloud.com>
2016-08-19 22:36:15 +0530

    default email ids updated

--- install/environment_variables.php
+++ install/environment_variables.php
@@ -33,9 +33,9 @@ putenv('aws_s3_bucket=bucket');
 
 putenv('ga=UA-XXXXXXXXX-1');
 
-putenv('email_postmaster=www@example.com');
+putenv('email_postmaster=no-reply@example.com');
 
-putenv('email_webmaster=founders@example.com,www@example.com');
+putenv('email_webmaster=founders@example.com,postmaster@example.com');
 putenv('email_admin=postmaster@example.com');
 
 putenv('api_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
